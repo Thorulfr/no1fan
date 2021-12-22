abe250eab1448072e1781098f087b4e227587658
maxh1231 <maxhumpherys@gmail.com>
2021-12-22 00:02:12 -0700

    post route for adding album favorites with hardcoded user_id

--- public/js/tracklist.js
+++ public/js/tracklist.js
@@ -10,10 +10,31 @@ let favBtnDeactive = function () {
     btn.classList.add('deactive');
 }
 
+let postDB = async (req, res) => {
+    let url = window.location.pathname;
+    let album_id = url.replace(/^\D+/g, '')
+    let album_name = document.getElementById('title').textContent;
+    const response = await fetch('/api/albumfavorites', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application.json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            album_id: album_id,
+            album_name: album_name,
+            user_id: 1
+        })
+    });
+    const postData = await response.json();
+    console.log(postData);
+}
+
 btn.addEventListener('click', function () {
     if (btn.classList.contains('active')) {
         favBtnDeactive();
     } else {
         favBtnActive();
+        postDB();
     }
 });
